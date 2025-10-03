9f450090245d2a33eb665a1d10d8463657ac3d0c
reyesjl <reyesjl.nine@hotmail.com>
2025-10-03 13:40:29 -0400

    no story: frontend vue setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="jumbotron">
+    <h1>Rugbycodex: <span class="highlight">Your Ultimate Rugby Resource</span></h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
