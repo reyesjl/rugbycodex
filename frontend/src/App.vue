416ef8623b00efa2b162fb8887c602c5f2e52509
reyesjl <reyesjl.nine@hotmail.com>
2025-10-29 14:15:30 -0400

    No Story - fix container problems

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -47,7 +47,7 @@ const downloadWhitepaper = () => {
 
 <template>
   <header class="fixed top-0 z-10 block w-full">
-    <nav class="max-w-6xl mx-auto flex justify-around items-center py-8">
+    <nav class="container flex justify-around items-center py-8">
       <!-- nav items -->
       <a class="text-neutral-500 hover:text-black dark:hover:text-white" href="#">Overview</a>
       <a class="text-neutral-500 hover:text-black dark:hover:text-white" href="#">Players</a>
@@ -63,9 +63,9 @@ const downloadWhitepaper = () => {
   </button>
 
   <!-- Hero section -->
-  <section class="hero container mx-auto flex min-h-screen flex-col items-center justify-center">
+  <section class="hero container flex min-h-screen flex-col items-center justify-center">
     <div class="text-neutral-400 mt-auto space-y-4">
-      <img :src="heroImg" alt="Rugbycodex" class="lg:mt-20 w-80 md:w-100 lg:w-105 max-w-120 h-auto mx-auto" />
+      <img :src="heroImg" alt="Rugbycodex" class="lg:mt-20 w-80 md:w-100 lg:w-105 max-w-120 h-auto mx-auto animate-pulse" />
       <img :src="heroWordbox" alt="Rugbycodex" class="w-80 md:w-100 lg:w-105 max-w-120 h-auto mx-auto" />
       <div class="text-center">Public Beta - Version 1.0</div>
     </div>
@@ -75,7 +75,7 @@ const downloadWhitepaper = () => {
   </section>
 
   <!-- Overview video -->
-  <section class="container mx-auto py-30 flex items-center justify-center">
+  <section class="container-lg py-30 flex items-center justify-center">
     <video class="w-80 md:w-3/4 h-auto border-10 md:border-12 border-neutral-700 rounded-xl" :src="videoDemo"
       playsinline autoplay muted loop preload="metadata">
       Your browser does not support the video tag.
@@ -83,7 +83,7 @@ const downloadWhitepaper = () => {
   </section>
 
   <!-- Hero statement -->
-  <section class="container mx-auto flex items-center justify-center text-center px-4 pb-32">
+  <section class="container flex items-center justify-center text-center pb-32">
     <div class="text-neutral-400 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl max-w-4xl leading-relaxed">
       Rugbycodex is an evolving system for understanding rugby.
       A platform where knowledge, footage, and context
@@ -92,28 +92,32 @@ const downloadWhitepaper = () => {
   </section>
 
 
-  <section class="container mx-auto w-full md:w-3/4 px-4">
+  <section class="container">
     <div class="micro-grid grid grid-cols-1 md:grid-cols-2 gap-16 text-neutral-900 dark:text-neutral-200">
       <div class="micro">
-        <div class="micro-title text-2xl font-semibold">Human-Centered.</div>
+        <div class="micro-line h-1 bg-neutral-300 dark:bg-neutral-700"></div>
+        <div class="micro-title text-3xl font-semibold mt-5">Human-Centered.</div>
         <div class="micro-description text-xl mt-10">Rugbycodex treats analysis as a conversation.
           Every clip, tag, and note is shared understanding between players and coaches.
           We believe technology should reveal the meaning behind play, not replace the human who gives it meaning.</div>
       </div>
       <div class="micro">
-        <div class="micro-title text-2xl font-semibold">Dynamic.</div>
-        <div class="micro-description text-lg mt-10">RugbyCodex is designed to capture the full context of rugby events.
+        <div class="micro-line h-1 bg-neutral-300 dark:bg-neutral-700"></div>
+        <div class="micro-title text-3xl font-semibold mt-5">Dynamic.</div>
+        <div class="micro-description text-lg mt-10">Rugbycodex is designed to capture the full context of rugby events.
           The system adapts to how each club, coach, or player learns. It flows naturally between footage, narrations,
           and annotations. We use the vocabulary players and coaches already know.</div>
       </div>
       <div class="micro">
-        <div class="micro-title text-2xl font-semibold">Intentional.</div>
-        <div class="micro-description text-lg mt-10">The modern rugby ecosystem is noisy. RugbyCodex reduces the clutter
+        <div class="micro-line h-1 bg-neutral-300 dark:bg-neutral-700"></div>
+        <div class="micro-title text-3xl font-semibold mt-5">Intentional.</div>
+        <div class="micro-description text-lg mt-10">The modern rugby ecosystem is noisy. Rugbycodex reduces the clutter
           to the essentials: play, learn,
           reflect. Every feature is designed to minimize friction and maximize insight.</div>
       </div>
       <div class="micro">
-        <div class="micro-title text-2xl font-semibold">Collective.</div>
+        <div class="micro-line h-1 bg-neutral-300 dark:bg-neutral-700"></div>
+        <div class="micro-title text-3xl font-semibold mt-5">Collective.</div>
         <div class="micro-description text-lg mt-10">Rugby isn’t built alone. Codex is an archive of intelligence,
           shaped by many voices like yours. Coaches, players, unions, and analysts. It evolves with you and the sport
           itself.
@@ -124,10 +128,10 @@ const downloadWhitepaper = () => {
 
   <div class="h-1 bg-neutral-400 my-32"></div>
 
-  <section class="container mx-auto w-full md:w-3/4 px-4">
+  <section class="container">
     <div class="chapter flex flex-col justify-center items-center text-neutral-400">
       <div class="text-4xl md:text-6xl text-neutral-400">Enduring</div>
-      <div class="text-xl md:text-3xl mt-10 text-center text-neutral-900 dark:text-neutral-200">RugbyCodex preserves coaching
+      <div class="text-xl md:text-3xl mt-10 text-center text-neutral-900 dark:text-neutral-200">Rugbycodex preserves coaching
         philosophy, player context, and tactical evolution for the next generation. A permanent digital record of how
         rugby is understood and the nuances of how it's played.</div>
 
