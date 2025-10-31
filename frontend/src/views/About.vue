ad940ff830a5f85403027db0de1f2684d73c9f93
reyesjl <reyesjl.nine@hotmail.com>
2025-10-31 10:24:30 -0400

    no story - about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -5,7 +5,7 @@
   <section class="hero container flex flex-col justify-center items-center min-h-screen">
     <div class="mt-auto text-5xl md:text-8xl text-amber-600">About</div>
     <p class="mb-auto text-center text-xl md:text-2xl mt-20 text-neutral-600 dark:text-neutral-300">
-      A special group of the game’s best thinkers and kindest hearts is building Rugbycodex. Coaches, union leaders, and people who believe rugby knowledge should be shared.
+      A special group of the game’s best thinkers and most grounded people are building Rugbycodex. Coaches, union leaders, and people who believe rugby knowledge should be shared.
     </p>
   </section>
 </template>
