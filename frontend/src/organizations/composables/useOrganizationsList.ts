7006cd773fdc4dbd3ab3cc6244e3cdb49dcd8368
Bennm23 <bennm23@gmail.com>
2025-11-19 15:51:26 -0500

    fix: refactor organization loading logic to use composable for better code organization

---
+++ frontend/src/organizations/composables/useOrganizationsList.ts
@@ -0,0 +1,30 @@
+import { getAllOrganizations, type Organization } from "@/services/org_service";
+import { ref } from 'vue';
+
+
+export function useOrganizationList() {
+  const organizations = ref<Organization[]>([]);
+  const loading = ref(false);
+  const error = ref<string | null>(null);
+
+  const loadOrganizations = async () => {
+    loading.value = true;
+    error.value = null;
+    try {
+      organizations.value = await getAllOrganizations();
+    } catch (e) {
+      error.value = e instanceof Error ? e.message : 'Failed to load organizations.';
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+
+    organizations,
+    loading,
+    error,
+
+    loadOrganizations,
+  }
+};
