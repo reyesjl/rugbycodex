9df8e58a61eaaa817345626d4f1dbd0751b20fc6
Bennm23 <bennm23@gmail.com>
2025-11-19 15:12:19 -0500

    fix: remove unused scrollbar styles from Releases and AdminOverview components

--- frontend/src/views/admin/AdminOverview.vue
+++ frontend/src/views/admin/AdminOverview.vue
@@ -188,13 +188,4 @@ onMounted(() => {
     padding-inline: 0;
   }
 }
-
-.no-scrollbar::-webkit-scrollbar {
-  display: none;
-}
-
-.no-scrollbar {
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-}
 </style>
