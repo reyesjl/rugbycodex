d9789ab9252808514dc44ed087a71c5f077d0f18
reyesjl <reyesjl.nine@hotmail.com>
2026-02-01 03:37:13 -0500

    chore: update version from v0.9.2-beta to v0.9.3

--- frontend/src/modules/app/components/Sidebar.vue
+++ frontend/src/modules/app/components/Sidebar.vue
@@ -183,7 +183,7 @@ void mobileSheetRef.value;
           </li>
         </ul>
         <div class="text-xs text-gray-400 px-4 py-3 mt-auto">
-          v0.9.2-beta
+          v0.9.3
         </div>
       </nav>
     </div>
@@ -334,7 +334,7 @@ void mobileSheetRef.value;
         </nav>
       </div>
       <div class="text-xs text-gray-400 px-4 py-3">
-        v0.9.2-beta
+        v0.9.3
       </div>
     </div>
   </div>
