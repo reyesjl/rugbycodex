b661b3edaae8ceb41e0ef0b3f27223d84692359a
reyesjl <reyesjl.nine@hotmail.com>
2026-02-04 12:48:27 -0500

    chore: bump version to v0.9.9 in package.json and Sidebar component

--- frontend/src/modules/app/components/Sidebar.vue
+++ frontend/src/modules/app/components/Sidebar.vue
@@ -183,7 +183,7 @@ void mobileSheetRef.value;
           </li>
         </ul>
         <div class="text-xs text-gray-400 px-4 py-3 mt-auto">
-          v0.9.8
+          v0.9.9
         </div>
       </nav>
     </div>
@@ -334,7 +334,7 @@ void mobileSheetRef.value;
         </nav>
       </div>
       <div class="text-xs text-gray-400 px-4 py-3">
-        v0.9.8
+        v0.9.9
       </div>
     </div>
   </div>
