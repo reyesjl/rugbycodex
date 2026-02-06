dd88830030284d157ca37108a140a984f306c2ff
reyesjl <reyesjl.nine@hotmail.com>
2026-02-06 11:28:58 -0500

    chore: update version from 0.10.4-beta to 0.10.5-beta in package.json and Sidebar.vue

--- frontend/src/modules/app/components/Sidebar.vue
+++ frontend/src/modules/app/components/Sidebar.vue
@@ -183,7 +183,7 @@ void mobileSheetRef.value;
           </li>
         </ul>
         <div class="text-xs text-gray-400 px-4 py-3 mt-auto">
-          v0.10.4-beta
+          v0.10.5-beta
         </div>
       </nav>
     </div>
@@ -334,7 +334,7 @@ void mobileSheetRef.value;
         </nav>
       </div>
       <div class="text-xs text-gray-400 px-4 py-3">
-        v0.10.4-beta
+        v0.10.5-beta
       </div>
     </div>
   </div>
