e509094c52cef5b944606994fecbf9c14b81ad84
reyesjl <reyesjl.nine@hotmail.com>
2026-02-06 14:05:41 -0500

    feat: update version label to include 'virtual swipe test' in Sidebar.vue

--- frontend/src/modules/app/components/Sidebar.vue
+++ frontend/src/modules/app/components/Sidebar.vue
@@ -183,7 +183,7 @@ void mobileSheetRef.value;
           </li>
         </ul>
         <div class="text-xs text-gray-400 px-4 py-3 mt-auto">
-          v0.10.5-beta virtual swipe test
+          v0.11.0-beta
         </div>
       </nav>
     </div>
@@ -334,7 +334,7 @@ void mobileSheetRef.value;
         </nav>
       </div>
       <div class="text-xs text-gray-400 px-4 py-3">
-       v0.10.5-beta virtual swipe test
+       v0.11.0-beta
       </div>
     </div>
   </div>
